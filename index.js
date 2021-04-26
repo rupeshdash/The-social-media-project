284882ac63efa9aa29f065aa3fa5b1290615b1d4
Rupesh Kumar Dash <rupeshkumardash123@gmail.com>
2021-04-26 19:19:50 +0530

    Setup the database

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+
+app.use(express.static('./assets'));
+
+//extract styles and scripts from subpages into the layout
+app.use(expressLayouts);
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
+
+
+
 
 //use express router
 app.use('/',require('./routes/index'));
