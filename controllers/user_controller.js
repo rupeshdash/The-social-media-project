9d4b8830fc70d5214578e9b2f6bb0d0a1b3b78ad
Rupesh Kumar Dash <rupeshkumardash123@gmail.com>
2021-04-28 19:57:43 +0530

    Add signup routes

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,9 +1,54 @@
-module.exports.profile = function(req,res){
-    return res.render('user_profile',{
-        title:"user_profile",
-        user:"Rupesh"
-    });
+const User = require("../models/user");
+module.exports.profile = function (req, res) {
+  return res.render("user_profile", {
+    title: "user_profile",
+    user: "Rupesh",
+  });
 };
 
+//render the sign in page
+module.exports.signIn = function (req, res) {
+  return res.render("user_sign_in", {
+    title: "Codiel | signIn",
+  });
+};
 
+//render the signUp page
+module.exports.signUp = function (req, res) {
+  return res.render("user_sign_up", {
+    title: "Codiel | signUp",
+  });
+};
 
+//get the sign up date
+module.exports.create = function (req, res) {
+  if (req.body.password == req.body.confirm_password) {
+    return res.redirect("back");
+  }
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (err) {
+      console.log("Error in finding user in signing up");
+      return;
+    }
+
+    //if user is not present then create a user.
+    if (!user) {
+      User.create(req.body, function (err, user) {
+        if (err) {
+          console.log("Error in creating user in signing up");
+          return;
+        }
+        return res.redirect("/users/sign-up");
+      });
+    }
+    
+    //if the email id you have used to create an user that already exist in the db then it redirect back to the page
+    else {
+      return res.redirect("back");
+    }
+  });
+};
+
+module.exports.createSession = function (req, res) {
+  //TODO later
+};
--- routes/users.js
+++ routes/users.js
@@ -5,4 +5,8 @@ const usersController = require('../controllers/user_controller');
 
 router.get('/profile',usersController.profile);
 
+router.get('/sign-in',usersController.signIn);
+router.get('/sign-up',usersController.signUp);
+router.post('/create',usersController.create);
+
 module.exports = router;
