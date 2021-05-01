4631b0ce0b9611c73dd6fa048637cae99bcfe2d6
Rupesh Kumar Dash <rupeshkumardash123@gmail.com>
2021-05-01 11:37:29 +0530

    Setup authentication using passport js

---
+++ config/passport-local-strategy.js
@@ -0,0 +1,69 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("../models/user");
+
+//authentication using passport
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+    },
+    function (email, password, done) {
+      //find a user and establish it's identity
+      User.findOne({ email: email }, function (err, user) {
+        if (err) {
+          console.log("error in finding user ----> Passport");
+          return done(err);
+        }
+
+        if (!user || user.password != password) {
+          console.log("Invalid user name and password");
+
+          // If the credentials are not valid (for example, if the password is incorrect), done should be invoked with false instead of a user to indicate an authentication failure.
+          return done(null, false);
+        }
+        // If the credentials are valid, the verify callback invokes done to supply Passport with the user that authenticated.
+        return done(null, user);
+      });
+    }
+  )
+);
+
+//serializing the user to decide which key is to be kept in the cookies
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+//deserializing the user from the key in the cookie
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    if (err) {
+      console.log("error in finding user ----> Passport");
+      return done(err);
+    } else {
+      return done(null, user);
+    }
+  });
+});
+
+//check fi the author is autheticated
+
+passport.chechAuthentication = function (req, res, next) {
+
+  if(req.isAuthenticated()){
+    return next();
+  }
+
+  return res.redirect('/users/sign-in');
+};
+
+passport.setAuthenticatedUser = function(req,res,next){
+    if(req.isAuthenticated()){
+      //req.user containes the current signed in user from the session cookie and we are
+      //just sending to the locals for views
+      res.locals.user = req.user;
+    }
+    next();
+}
+
+module.exports = passport;
--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,13 +1,16 @@
 const User = require("../models/user");
 module.exports.profile = function (req, res) {
   return res.render("user_profile", {
-    title: "user_profile",
-    user: "Rupesh",
+    title: "User profile",
+    
   });
 };
 
 //render the sign in page
 module.exports.signIn = function (req, res) {
+  if(req.isAuthenticated()){
+    return res.redirect('/users/profile');
+  }
   return res.render("user_sign_in", {
     title: "Codiel | signIn",
   });
@@ -15,6 +18,10 @@ module.exports.signIn = function (req, res) {
 
 //render the signUp page
 module.exports.signUp = function (req, res) {
+  if(req.isAuthenticated()){
+    return res.redirect('/users/profile');
+  }
+
   return res.render("user_sign_up", {
     title: "Codiel | signUp",
   });
@@ -51,4 +58,9 @@ module.exports.create = function (req, res) {
 
 module.exports.createSession = function (req, res) {
   //TODO later
+  return res.redirect('/')
 };
+module.exports.destroySession = function(req,res){
+  req.logout();
+  return res.redirect('/');
+}
