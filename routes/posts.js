2bfbda128406db774e404708cfc2b0c4c8879332
Rupesh Kumar Dash <rupeshkumardash123@gmail.com>
2021-05-04 11:26:49 +0530

    Set only  authenticated logged in user to post on the feed

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 
 const postController = require('../controllers/posts_controller');
 
-router.post('/create',postController.create);
+//only signed in users can post into the feed
+router.post('/create',passport.checkAuthentication,postController.create);
 // router.get('/display',postController.display);
 
 module.exports = router;
