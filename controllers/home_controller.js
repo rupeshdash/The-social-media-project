1575b13a142cfe5ec1489db4f624879405a7506f
Rupesh Kumar Dash <rupeshkumardash123@gmail.com>
2021-05-05 07:17:34 +0530

    Show comments and user commented below the posts

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -9,7 +9,15 @@ module.exports.home = function(req,res){
     // });
 
     //populate the user of each posts
-    Post.find({}).populate('user').exec(function(err,posts){
+    Post.find({})
+    .populate('user')
+    .populate({
+        path:'comments',
+        populate:{
+            path:'user'
+        }
+    })
+    .exec(function(err,posts){
         res.render('home',{
             title:'Codeial | Home',
             posts:  posts
